66beeaf0e6211dc66aa048f2bf793773e7b48f2d
Raul Zeferino <raul.r.ze@hotmail.com>
2018-06-05 11:01:35 -0300

    Added favorite FlatList in home screen.

--- app/screens/main/home/Home.js
+++ app/screens/main/home/Home.js
@@ -10,6 +10,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Image source={require('../../../images/logos/heyceitas.png')} style={styles.logo}/>
         <Favorites/>
         <View style={styles.footer}>
         <Text>BUSCAS RECENTES></Text>
@@ -19,16 +20,41 @@ export default class Home extends React.Component {
   }
 }
 
+class Post extends React.Component {
+  render(){
+    return (<View style={{width:300, height:'100%', margin:5, backgroundColor:'white',alignItems:'center',paddingTop:5,paddingLeft:2.5,paddingBottom:5,paddingRight:2.5}}>
+    <Image style={{width:'100%', height:'100%',borderRadius:5}} source={this.props.img}/>
+    <View style={{position:'absolute',left:2.5,right:2.5,top:5,bottom:5,alignItems:'center',justifyContent:'center',borderRadius:5,backgroundColor:'rgba(0,0,0,.3)'}}><Text style= {styles.name}>{this.props.recipename}</Text></View>
+    </View>)
+  }
+}
+
 class Favorites extends React.Component {
   render(){
-    let dataHERE = [{"name":"teste1"},{"name":"teste2"},{"name":"teste3"},{"name":"teste4"},{"name":"teste5"},{"name":"teste6"},{"name":"teste7"},{"name":"teste8"},{"name":"teste9"},{"name":"teste10"}]
+    let dataHERE = [
+      {"image": require('../../../images/category/brazilian.jpg'), "name":"Brasileiras"},
+      {"image": require('../../../images/category/candy.jpg'), "name":"Doces"},
+      {"image": require('../../../images/category/chicken.jpg'), "name":"Frango"},
+      {"image": require('../../../images/category/french.jpg'),"name":"Francesa"},
+      {"image": require('../../../images/category/italian.jpg'),"name":"Italiana"},
+      {"image": require('../../../images/category/juices.jpg'),"name":"Sucos"},
+      {"image": require('../../../images/category/meat.jpg'),"name":"Carnes"},
+      {"image": require('../../../images/category/pastas.jpg'),"name":"Massas"},
+      {"image": require('../../../images/category/practices.jpg'),"name":"Práticas"},
+      {"image": require('../../../images/category/salty_fit.jpg'),"name":"Salgados Fit"},
+      {"image": require('../../../images/category/salty_vegan.jpg'),"name":"Salgados Vegano"},
+      {"image": require('../../../images/category/soups.jpg'),"name":"Sopas"},
+      {"image": require('../../../images/category/sweet_fit.jpg'),"name":"Doces Fit"},
+      {"image": require('../../../images/category/sweet_vegan.jpg'),"name":"Doces Vegano"},
+      {"image": require('../../../images/category/vegetarian.jpg'),"name":"Vegetariana"}
+    ]
 
     return(
     <View style={styles.favorites}>
     <FlatList
     horizontal={true}
     data={dataHERE}
-    renderItem={({item})=> <Text style={styles.text}>{item.name}</Text>}
+    renderItem={({item})=> <Post img={item.image} recipename={item.name}/>}
     />
     </View>
     );
@@ -40,11 +66,12 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
   favorites:{
-    flex:3,
-backgroundColor:'#FCFCFC'
+    flex:2,
+    marginTop:-20
   },
 footer: {
   flex:2,
@@ -63,5 +90,15 @@ icon:{
 text: {
   fontSize:30,
   padding:30
+},
+logo:{
+height:60,
+width:200,
+margin:15
+},
+name: {
+  fontSize:26,
+  fontWeight:'bold',
+  color:'white',
 }
 });
